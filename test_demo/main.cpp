3db1be0d837cab9a9a3e0d7c8e093479f700aff1
heping.hm <southwindod@foxmail.com>
2022-04-07 21:47:02 +0800

    整理工程

---
+++ test_demo/main.cpp
@@ -0,0 +1,13 @@
+// main.cpp : Defines the entry point for the console application.
+//
+
+#include <SDKDDKVer.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <Windows.h>
+
+int main()
+{
+	system("pause");
+	return 0;
+}
